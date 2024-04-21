8cd3958995a199b5bc328b7a127ff3409456fd83
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-04-21 14:09:52 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,9 +1,22 @@
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    let a = 1;
+    for (let i = 2; i <= n; i++) {
+        a *= i;
+    }
+    return a;
 }
 
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    if(n === 0) {
+        return 1;
+    } 
+
+    else {
+        return 1.0 / factorial(n) + e(n - 1);
+    } 
 }
+
+//n = 4;
+//result = e(n);
+//console.log(result);
+
--- code.test.js
+++ code.test.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 const assert = require('assert');
+const jsc = require('jsverify');
 eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+function factorialTest(n) {
+  if(n === 0) return 1;
+  else return n * factorialTest(n - 1);
+}
+
+function eTest(n) {
+  if(n === 0) return 1;
+  else return 1.0 / factorialTest(n) + e(n - 1);
+}
+
+const test =
+    jsc.forall("nat", function(n) {
+        return JSON.stringify(e(n)) ==
+            JSON.stringify(eTest(n));
+    });
+jsc.assert(test);
+console.log('tests passed');
